71e99fbfcce3edd68453bbaf775419684cb39e7c
vladislav.tsvek <vladislav.tsvek@V-TSVEK>
2016-09-20 17:15:26 +0300

    Add project files.

---
+++ Get selected text or HTML for the frame WPFBrowser/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Get_selected_text_or_HTML_for_the_frame_WPFBrowser
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
